0bea57e43d2614d7ddf3b41cd79160313fb8d05a
Artemides <edmpulasky@gmail.com>
2023-06-08 22:33:00 -0500

    create cards

---
+++ src/Components/Card.tsx
@@ -0,0 +1,10 @@
+import { type } from "os";
+import React from "react";
+
+export const Card = () => {
+  return (
+    <div className="w-1/2 p-4 bg-gray-950 shadow-sm rounded-xl text-white">
+      Card
+    </div>
+  );
+};
