1d4780e4cfe22a9f46ba0c14e26b0c3535cfe585
Artemides <edmpulasky@gmail.com>
2023-06-08 23:30:48 -0500

    update ui

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,10 +1,16 @@
 import { type } from "os";
-import React from "react";
+import React, { ReactNode } from "react";
 
-export const Card = () => {
+type CardProps = {
+  title: string;
+  children: ReactNode;
+};
+
+export const Card = ({ title, children }: CardProps) => {
   return (
-    <div className="w-1/2 p-4 bg-gray-950 shadow-sm rounded-xl text-white">
-      Card
+    <div className="w-1/2 p-4 bg-gray-950 shadow-sm rounded-xl ">
+      <h3 className=" text-white text-2xl text-center font-bold">{title}</h3>
+      {children}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,43 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex  gap-4 p-16 bg-gradient-to-r from-slate-950 from-10% via-slate-900 via-40% to-black to-80% ">
-      <Card />
-      <Card />
+    <main className="w-screen h-screen flex items-start  gap-4 p-16 bg-gradient-to-r from-slate-950 from-10% via-slate-900 via-40% to-black to-80% ">
+      <Card title="Your Wallet">
+        <div className="p-6 flex flex-col">
+          <label>
+            <span className="text-white">Address</span>
+            <input
+              type="text"
+              className="custom-input"
+              placeholder="0xksd89213jkasd98213kjas..."
+            />
+          </label>
+          <div className="flex items-center justify-center self-center bg-sky-500/10 rounded-full mt-4 px-12 py-3 ">
+            <p className="text-white text">
+              Balance: <span className="font-bold">0</span>
+            </p>
+          </div>
+        </div>
+      </Card>
+      <Card title="Transaction">
+        <div className="p-6 flex flex-col gap-6">
+          <label>
+            <span className="text-white">Recipient</span>
+            <input
+              type="text"
+              className="custom-input"
+              placeholder="0xksd89213jkasd98213kjas..."
+            />
+          </label>
+          <label>
+            <span className="text-white">Ammout</span>
+            <input type="text" className="custom-input" placeholder="0" />
+          </label>
+          <button className="py-2 px-6 self-center text-sky-500 font-semibold bg-sky-500/25 rounded-full hover:bg-sky-500/40 transition-colors ease-out duration-300">
+            Transfer
+          </button>
+        </div>
+      </Card>
     </main>
   );
 }
