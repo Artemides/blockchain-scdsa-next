8bf20c43d9b62ffcd9fcc5ec08ef09440e100220
Artemides <edmpulasky@gmail.com>
2023-06-09 10:38:18 -0500

    include moralis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import { Alert } from "@/Components/Alert";
+import { MoralisProvider } from "react-moralis";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="font-sans">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <MoralisProvider initializeOnMount={false}>
+          <Navbar />
+          {children}
+        </MoralisProvider>
       </body>
     </html>
   );
